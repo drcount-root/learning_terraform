0c4ce6f592a6eb9b432941f7458c1d082f4b5bcc
Subham Sahu <about.subhamsahu@gmail.com>
2026-02-01 23:56:12 +0530

    ..

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.30.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region     = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
